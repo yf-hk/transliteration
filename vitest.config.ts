70967a680a5b16c20b6d077282ca15229cb532e3
Eric Hu <admin@makerhub.cn>
2025-12-16 17:04:54 +0800

    chore: fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
       include: ['src/**/*.ts'],
-      exclude: ['src/cli/**/*.ts', 'data/**/*.ts'],
+      exclude: ['src/cli/**/*.ts', 'src/types/**/*.ts', 'data/**/*.ts'],
     },
   },
 });
